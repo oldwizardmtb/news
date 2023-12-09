38565ec603d0546af33283f56962f162b082cafa
old <sobirjon>
2023-12-09 11:28:16 +0500

    first commit

---
+++ news/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import News
+
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    save_on_top = True
+
+
+admin.site.register(News, NewsAdmin)
